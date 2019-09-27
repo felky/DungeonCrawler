a5126dd9afbbe9c4df5378c204b84770dd9818a8
Linus Alpsten <linus.alpsten@telia.com>
2019-09-27 10:02:20 +0200

    add wall

---
+++ DungeonCrawler/DungeonCrawler/Wall.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonCrawler
+{
+    class Wall : Tile
+    {
+        public override char tile { get => '#'; }
+
+        public override void Interact(Player player)
+        {
+
+        }
+    }
+}
