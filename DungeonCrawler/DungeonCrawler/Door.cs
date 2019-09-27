47952a078633885d0bcfb8c72215ac8a4dce3ad9
Felix Gramell <felixg.nti@gmail.com>
2019-09-27 11:10:46 +0200

    changed chars when interacting

--- DungeonCrawler/DungeonCrawler/Door.cs
+++ DungeonCrawler/DungeonCrawler/Door.cs
@@ -20,6 +20,7 @@ namespace DungeonCrawler
                 {
                     player.Keys--;
                     locked = false;
+                    t = '.';
                 }
             }
             if (!locked)
