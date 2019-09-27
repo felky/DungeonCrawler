69c691144dd52f2f397630b086ba4183231605dd
Felix Gramell <felixg.nti@gmail.com>
2019-09-27 10:20:56 +0200

    Added files

---
+++ DungeonCrawler/DungeonCrawler/Exit.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonCrawler
+{
+    class Exit : Tile
+    {
+        public override char tile { get => 'U'; }
+
+        public override void Interact(Player player)
+        {
+            End();
+        }
+
+        private void End()
+        {
+            Console.Clear();
+            Console.WriteLine("Congratulations, you did it!");
+            Console.ReadLine();
+            Environment.Exit(0);
+        }
+    }
+}
--- DungeonCrawler/DungeonCrawler/Room.cs
+++ DungeonCrawler/DungeonCrawler/Room.cs
@@ -4,42 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-public enum Type { Monster, Key, Empty, Door, Exit }
-
 namespace DungeonCrawler
 {
     public class Room : Tile
     {
-        private Type _type; 
-
-        public Room(Type type)
-        {
-            _type = type;
-        }
-
+        public override char tile { get => '.'; }
         public override void Interact(Player player)
         {
-            switch (_type)
-            {
-                case Type.Empty:
-                    tile = '.';
-                    break;
-                case Type.Key:
-                    tile = 'k';
-                    break;
-                case Type.Door:
-                    tile = 'D';
-                    break;
-                case Type.Exit:
-                    tile = 'U';
-                    break;
-                case Type.Monster:
-                    tile = 'M';
-                    break;
-                default:
-                    break;
-            }
+            player.Move();
         }
     }
 }
-
