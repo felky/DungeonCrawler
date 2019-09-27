2a349d5e9aa93c1e1e6452700c7cec4e394114a2
Felix Gramell <felixg.nti@gmail.com>
2019-09-27 09:56:07 +0200

    Update Room.cs

--- DungeonCrawler/DungeonCrawler/Room.cs
+++ DungeonCrawler/DungeonCrawler/Room.cs
@@ -1,8 +1,14 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-public class Room
+namespace DungeonCrawler
 {
-	public Room()
-	{
-	}
+    public class Room 
+    {
+
+    }
 }
+
